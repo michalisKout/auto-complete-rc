70899a627a26362ca129c693ee1c17b6d7ac7550
Michalis Koutridis <michalis.koutridis@gmail.com>
2025-05-27 11:13:46 +0300

    build: Scaffold react app for auto complete component
    - Use create react app script from Vite
    - Setup basic rules for biome for linting
    - Add basic css for index page

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <main className="app__main">
+        <h1 className="app__title">React Autocomplete Assignment</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
